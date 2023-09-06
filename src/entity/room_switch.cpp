c9f5895b2e122a3a475f00582573bf54fd8c0178
JustDoom <61824552+JustDoom@users.noreply.github.com>
2023-09-06 15:11:37 +1000

    start room switch

---
+++ src/entity/room_switch.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dino on 6/09/23.
+//
+
+#include "room_switch.h"
---
+++ src/entity/room_switch.h
@@ -0,0 +1,16 @@
+//
+// Created by dino on 6/09/23.
+//
+
+#ifndef TEXTDUNGEON_ROOM_SWITCH_H
+#define TEXTDUNGEON_ROOM_SWITCH_H
+
+#include "entity.h"
+
+class RoomSwitch : Entity {
+private:
+public:
+};
+
+
+#endif //TEXTDUNGEON_ROOM_SWITCH_H
